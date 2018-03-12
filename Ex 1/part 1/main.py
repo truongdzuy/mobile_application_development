948b774ed60d8e7e1d9be065e0dc800e2e5a131b
Truong Dang <dangdzuytruong@gmail.com>
2018-03-12 13:45:22 +0700

    update part 1

--- Ex 1/part 1/main.py
+++ Ex 1/part 1/main.py
@@ -3,6 +3,6 @@ apiKey = 'AIzaSyDgoY6l7wIw2ekbf2aYnCjUyYTKxFVG9p0'
 lat = input("Please enter latitude: ")
 lng = input("Please enter longitude: ")
 maps = Client(apiKey)
-reverseGeo = maps.reverse_geocode(lat,lng)
-address = reverseGeo['Placemark'][0]['address']
+reverseGeo = maps.reverse_geocode((lat,lng))
+address = reverseGeo[0]['formatted_address']
 print(address)
