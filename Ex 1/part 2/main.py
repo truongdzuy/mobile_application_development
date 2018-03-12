72bcad119bfac90e10e17eda09f6cbe3d5982612
Truong Dang <dangdzuytruong@gmail.com>
2018-03-12 15:19:53 +0700

    update part 2

--- Ex 1/part 2/main.py
+++ Ex 1/part 2/main.py
@@ -1,10 +1,19 @@
 from googlemaps import Client
-apiKey = 'AIzaSyDP0pM9ag___m0xGd6peTDdVRTGyjRgv80'
-lat1 = input("Please enter latitude for location A: ")
-lng1 = input("Please enter longitude for location A: ")
-lat2 = input("Please enter latitude for location B: ")
-lng2 = input("Please enter longitude for location B: ")
-maps = Client(apiKey)
-distMatrix = maps.distance_matrix((lat1,lng1),(lat2,lng2))
-dist = distMatrix['rows'][0]['elements'][0]['distance']['text']
-print("Distance: " + dist)
+import math
+
+def distCal(lat1, lng1, lat2, lng2):
+    earthRad = 6371
+    latDis = math.radians(lat2 - lat1)
+    lngDis = math.radians(lng2 - lng1)
+    a = math.pow(math.sin(latDis / 2),2) \
+        + math.pow(math.sin(lngDis / 2),2) \
+        * math.cos(math.radians(lat1))*math.cos(math.radians(lat2))
+    c = 2 * math.atan2(math.sqrt(a),math.sqrt(1-a))
+    check = earthRad * c
+    return check
+lat1 = float(input("Please enter latitude for location A: "))
+lng1 = float(input("Please enter longitude for location A: "))
+lat2 = float(input("Please enter latitude for location B: "))
+lng2 = float(input("Please enter longitude for location B: "))
+dist = round(distCal(lat1, lng1, lat2, lng2), 3)
+print("Distance: " + str(dist) + "km")
